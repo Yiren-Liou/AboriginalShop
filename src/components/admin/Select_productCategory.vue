a5ad98fee61e98ebcd21aa60f005a969b0f6d1ce
Yiren <liou830129@gmail.com>
2021-07-01 18:59:36 +0800

    [刪除] 商品分類 isReadonly

--- src/components/admin/Select_productCategory.vue
+++ src/components/admin/Select_productCategory.vue
@@ -1,38 +1,20 @@
 <template>
   <div class="col-md-6 mb-3">
     <label for="mainCategory" class="form-label">分類<sup>*</sup></label>
-      <select v-if="add" id="mainCategory" class="form-select"
-            v-model="mainIndex" @change="resetSubIndex">
-      <option value="" disabled>請選擇一個分類</option>
-      <option v-for="(item) in mainCategory"
-      :key=item :value=item> {{ item }} </option>
-    </select>
-    <select v-else id="mainCategory" class="form-select"
-            :disabled="isReadonly"
-            v-model="tempMain" @change="resetSubIndex">
-      <option value="" disabled>請選擇一個分類</option>
-      <option v-for="(item) in mainCategory" :key='item'
-              :value='item' :selected="item === 'tempMain'">
-              {{ item }}
-      </option>
-    </select>
+      <select id="mainCategory" class="form-select"
+              v-model="tempMain" @change="resetSubIndex" :disabled="readonly">
+        <option value="" disabled>請選擇一個分類</option>
+        <option v-for="(item) in mainCategory"
+        :key=item :value=item> {{ item }} </option>
+      </select>
   </div>
   <div class="col-md-6 mb-3">
     <label for="subCategory" class="form-label">次分類<sup>*</sup></label>
-    <select v-if="add" id="subCategory" class="form-select"
-            v-model="subIndex" @change="emitCategory()">
+    <select id="subCategory" class="form-select"
+            v-model="tempSub" @change="emitCategory()" :disabled="readonly">
       <option value="" disabled>請選擇一個次分類</option>
       <option v-for="(item) in sub" :key=item
               :value=item>{{ item }}
-      </option>
-    </select>
-    <select v-else :disabled="isReadonly"
-            id="subCategory" class="form-select"
-            v-model="tempSub" @change="emitCategory()">
-      <option value="" disabled>請選擇一個次分類</option>
-      <option v-for="(item) in sub" :key='item'
-              :value='item' :selected="item === 'tempSub'">
-              {{ item }}
       </option>
     </select>
   </div>
@@ -44,14 +26,11 @@ export default {
     return {
       tempMain: '',
       tempSub: '',
-      mainIndex: '',
-      subIndex: '',
       category: {
         新鮮蔬果: ['水果', '蔬菜', '手工果醬'],
         沖泡飲品: ['茶葉', '咖啡'],
         手工編織: ['圍巾', '置物籃'],
       },
-      isReadonly: '',
     };
   },
   props: ['readonly', 'editMain', 'editSub', 'add'],
@@ -63,26 +42,17 @@ export default {
     editSub() {
       this.tempSub = this.editSub;
     },
-    readonly() {
-      this.isReadonly = this.readonly;
-    },
   },
   computed: {
     mainCategory() {
       return Object.keys(this.category);
     },
     sub() {
-      if (this.add) {
-        return this.category[this.mainIndex];
-      }
       return this.category[this.tempMain];
     },
   },
   methods: {
     resetSubIndex() {
-      if (this.add) {
-        this.subIndex = '';
-      }
       this.tempSub = '';
     },
     emitCategory() {
