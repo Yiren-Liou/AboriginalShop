e3fb0565eee1994588bc191bda2f5e96079b192d
Yiren <liou830129@gmail.com>
2021-07-23 13:15:01 +0800

    [新增] 聯絡我們

--- src/views/front/Contact.vue
+++ src/views/front/Contact.vue
@@ -1,13 +1,80 @@
 <template>
-  聯絡我們
+  <div class="main container-fluid">
+    <div class="row justify-content-center mb-6">
+      <div class="col-md-5">
+        <h2 class="fontSizeL text-center mb-5">聯絡我們</h2>
+        <ul class="list-unstyled text-center mb-4">
+          <li class='mb-3'>
+            如果需要大量訂購，或任何問題，歡迎您來電( <a href="tel:+886-2-12345678"
+             class="text-secondary fw-bold text-decoration-underline">02-1234-5678</a> )洽詢。
+          </li>
+          <li>
+            不方便來電，可以透過下方郵件詢問，我們會在 2 個工作天內回覆您。
+          </li>
+        </ul>
+        <Form v-slot="{ errors }" @submit='sendEmail'
+              class="rounded-3 border border-secondary py-4 py-lg-5">
+          <div class="row justify-content-center mb-3 mb-lg-4">
+            <label for="userName" class="col-sm-2 col-form-label">姓名<sup>*</sup></label>
+            <div class="col-sm-8">
+              <Field type="text" class="form-control disabled" id="userName"
+                  placeholder="請輸入姓名"
+                  name="姓名" rules="required"
+                  :class="{ 'is-invalid': errors['姓名'] }">
+              </Field>
+              <error-message name="姓名" class="invalid-feedback"></error-message>
+            </div>
+          </div>
+          <div class="row justify-content-center mb-3 mb-lg-4">
+            <label for="userName" class="col-sm-2 col-form-label">Email<sup>*</sup></label>
+            <div class="col-sm-8">
+              <Field type="email" class="form-control disabled" id="userName"
+                  placeholder="請輸入Email"
+                  name="Email" rules="required"
+                  :class="{ 'is-invalid': errors['Email'] }">
+              </Field>
+              <error-message name="Email" class="invalid-feedback"></error-message>
+            </div>
+          </div>
+          <div class="row justify-content-center mb-5">
+            <label for="userName" class="col-sm-2 col-form-label">詢問事項<sup>*</sup></label>
+            <div class="col-sm-8">
+              <Field type="text" class="form-control disabled" id="userName"
+                  placeholder="請輸入詢問事項"
+                  name="content" rules="required"
+                  :class="{ 'is-invalid': errors['content'] }"
+                  as="textarea"
+                  style="height: 160px">
+              </Field>
+              <error-message name="content" class="invalid-feedback"></error-message>
+            </div>
+          </div>
+          <div class="d-center">
+            <button type="submit" class='btn btn-primary'>送出</button>
+          </div>
+        </Form>
+      </div>
+    </div>
+  </div>
+  <Loading :active="isLoading">
+    <div class="loadingio-spinner-dual-ball-haac1tizt7t"><div class="ldio-u3364un719">
+    <div></div><div></div><div></div>
+    </div></div>
+  </Loading>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      isLoading: false,
+    };
   },
   mounted() {
+    this.isLoading = true;
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 2000);
     const topNav = document.querySelector('#topNav');
     if (topNav.classList.contains('show')) {
       topNav.classList.remove('show');
